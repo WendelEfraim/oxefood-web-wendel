790eec29e0827272dd192a6d30bdeb842bb4df9e
WendelEfraim <wendel.efrain11@gmail.com>
2024-11-21 15:57:29 -0300

    conectar com back

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
 // import Home from './views/home/Home';
-// import FormCliente from './views/client/FormCliente';
+import FormCliente from './views/client/FormCliente';
 // import FormProduto from './views/produto/FormProduto';
-import FormEntregador from './views/entregador/FormEntregador';
+// import FormEntregador from './views/entregador/FormEntregador';
 
 function App() {
   return (
     <div className="App">
-         <FormEntregador />
+         <FormCliente />
 
           <div style={{marginTop: '6%'}}>
             <Segment vertical color='grey' size='tiny' textAlign='center'>
