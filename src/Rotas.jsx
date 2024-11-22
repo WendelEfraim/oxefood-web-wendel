3a43eca7fbf31e91829a44e87484542d7d499b8e
WendelEfraim <wendel.efrain11@gmail.com>
2024-11-22 14:07:21 -0300

    emplementação de listagem clientes

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -5,6 +5,7 @@ import FormCliente from './views/client/FormCliente';
 import FormEntregador from './views/entregador/FormEntregador';
 import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
+import ListCliente from './views/client/ListCliente';
 
 function Rotas() {
     return (
@@ -14,6 +15,7 @@ function Rotas() {
                 <Route path="form-cliente" element={ <FormCliente/> } />
                 <Route path="form-produto" element={ <FormProduto/> } />
                 <Route path="form-entregador" element={ <FormEntregador/> } />
+                <Route path="list-cliente" element={ <ListCliente/> } />
             </Routes>
         </>
     )
